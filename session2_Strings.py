3e867b572ec106590e4e21862545a09620376e2f
khqhq <nguyenquockhanh3994@gmail.com>
2016-11-28 17:29:10 +0700

    session2_Strings

---
+++ session2_Strings.py
@@ -0,0 +1,31 @@
+
+#1
+color_list = ['Blue', 'Red','Black', 'Pink', 'Brown', 'Yellow']
+print("My color list",(color_list))
+
+
+#2
+print("color_list at index 3 is {0}".format(color_list[3]))
+
+#3
+i = int(input("enter a number from 0-5: "))
+print("your color:",color_list[i])
+
+print()
+#4
+
+color_list = \
+[
+	"Blue",
+	"Red",
+	"Black",
+	"Pink",
+        "Brown",
+        "Yellow"
+]
+ 
+for i in color_list:
+	print (i)
+
+
+#5Em_chua_nghi_ra
